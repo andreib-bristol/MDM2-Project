321cecb5b19d78ba44116f806ccb0c1abed0745e
Jamesdunl <ir24312@bristol.ac.uk>
2026-03-02 15:29:37 +0000

    code
    opens 2 files and find the dot product of the unit vectors. Need to now add code to iterate through time delays to get the maximum average cos(theta)

--- pigeon.py
+++ pigeon.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 
 file1 = "pigeonflocks_trajectories/ff1/ff1_A.txt"
 file2 = "pigeonflocks_trajectories/ff1/ff1_F.txt"
@@ -12,4 +14,36 @@ columns = ["t","x", "y", "z","vx", "vy", "vz","ax", "ay", "az","gps"]
 data1.columns = columns
 data2.columns = columns
 
-print(data1.head)
+t_start = max(data1["t"].min(), data2["t"].min())
+t_end   = min(data1["t"].max(), data2["t"].max())
+
+d1 = data1[(data1["t"] >= t_start) & (data1["t"] <= t_end)].reset_index(drop=True)
+d2 = data2[(data2["t"] >= t_start) & (data2["t"] <= t_end)].reset_index(drop=True)
+
+#print(d1.shape, d2.shape)
+
+
+
+def unit_heading(data):
+    v = data[["vx", "vy", "vz"]].to_numpy()
+    #print(v[:5])
+    speed = np.sqrt(np.sum(v**2, axis=1))
+    #print(speed[:5])
+    speed[speed == 0] = np.nan
+    u = v / speed[:, None]
+    return u
+
+
+u1 = unit_heading(d1)
+u2 = unit_heading(d2)
+
+
+#print(u1.shape, u2.shape)
+
+
+dots = np.sum(u1 * u2, axis=1)
+C0 = np.nanmean(dots)
+
+print("C_ij(0) =", C0)
+print("first 5 dot products:", dots[:5])
+
